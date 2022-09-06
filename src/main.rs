a11a2cf74074917d68fa1f69c88cab084dda6e42
BuildTools <unconfigured@null.spigotmc.org>
2022-09-06 19:58:30 -0400

    zsc

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,42 @@
 #![windows_subsystem = "windows"]
 
 use std::process::{Command};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{process, thread, time};
+use sysinfo::Process;
 
 #[tokio::main]
 async fn main() {
-    let mut time = get_time_millis();
+    let five = time::Duration::from_secs(5);
     let mut value:bool;
-    loop {
-        if get_time_millis() < time + 5000 { continue; }
 
+    loop {
         let response = reqwest::get("http://97.107.130.20:8888/value").await;
         if response.is_err() {
-            time = get_time_millis();
+            thread::sleep(five);
             continue;
         }
+
+
         let text = response.unwrap().text().await.unwrap();
         if text.eq("yes") { value = true; }
         else { value = false; }
 
-        if !value { kill_valorant() }
-
-        time = get_time_millis();
+        if !value {
+            /*Command::new("taskkill")
+                .arg("/IM")
+                .arg("VALORANT-Win64-Shipping.exe")
+                .arg("/F")
+                .spawn().expect("failed");
+            Command::new("taskkill")
+                .arg("/IM")
+                .arg("VALORANT.exe")
+                .arg("/F")
+                .spawn().expect("failed");*/
+        }
+        thread::sleep(five);
     }
 }
 
-fn kill_valorant() {
-    Command::new("taskkill")
-        .arg("/IM")
-        .arg("VALORANT-Win64-Shipping.exe")
-        .arg("/F")
-        .output()
-        .expect("Failed to execute process");
-    Command::new("taskkill")
-        .arg("/IM")
-        .arg("VALORANT.exe")
-        .arg("/F")
-        .output()
-        .expect("Failed to execute process");
-}
-
-fn get_time_millis() -> u128 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards").as_millis()
+fn asd(name: String) -> u32 {
+    sysinfo::Process::
 }
