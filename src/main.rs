11c9ecd0062df57cf9fc8a5af294c667ff89bb76
BuildTools <unconfigured@null.spigotmc.org>
2022-09-07 15:56:37 -0400

    uhiudshiuadhiusd

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 #![windows_subsystem = "windows"]
 
-use std::process::{Command};
-use std::{process, thread, time};
-use sysinfo::Process;
+use std::{thread, time};
+use sysinfo::{PidExt, ProcessExt, SystemExt};
+use windows::Win32::System::Threading::{OpenProcess, PROCESS_TERMINATE, TerminateProcess};
 
 #[tokio::main]
 async fn main() {
+    let mut system = sysinfo::System::new();
+    system.refresh_all();
+
     let five = time::Duration::from_secs(5);
     let mut value:bool;
-
     loop {
         let response = reqwest::get("http://97.107.130.20:8888/value").await;
         if response.is_err() {
@@ -16,27 +18,25 @@ async fn main() {
             continue;
         }
 
-
         let text = response.unwrap().text().await.unwrap();
         if text.eq("yes") { value = true; }
         else { value = false; }
 
         if !value {
-            /*Command::new("taskkill")
-                .arg("/IM")
-                .arg("VALORANT-Win64-Shipping.exe")
-                .arg("/F")
-                .spawn().expect("failed");
-            Command::new("taskkill")
-                .arg("/IM")
-                .arg("VALORANT.exe")
-                .arg("/F")
-                .spawn().expect("failed");*/
+            let ps = system.processes().iter().filter(|(_, p)| p.name().starts_with("VALORANT"));
+            for (pid, _p) in ps {
+                //println!("{}:{}", pid, p.name());
+                kill_process(pid.as_u32());
+            }
         }
+        system.refresh_processes();
         thread::sleep(five);
     }
 }
 
-fn asd(name: String) -> u32 {
-    sysinfo::Process::
+fn kill_process(pid: u32) {
+    unsafe {
+        let handy = OpenProcess(PROCESS_TERMINATE, true, pid).unwrap();
+        TerminateProcess(handy, 0);
+    }
 }
